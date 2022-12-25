20284c6a07ccb0b2ee5ab7e5a04e786708bd786a
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2022-12-25 03:57:06 +0000

    Delete Libre directory

--- Libre/librespeedtest/backend/getIP_ipInfo_apikey.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// put your token between the quotes if you have one
-$IPINFO_APIKEY = '';
--- Libre/librespeedtest/results/telemetry_settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// If set to true, IP addresses will be redacted from IP and ISP info fields, as well as the log
-$redact_ip_addresses = false;
-
-// Sqlite3 settings
-$Sqlite_db_file = '/var/www/localhost/htdocs/results/data/librespeedtest_telemetry.db';
